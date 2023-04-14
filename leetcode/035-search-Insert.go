37bb880f3e1b2cc01453a4be47545203d68e87cb
SM1701 <717513736@qq.com>
2023-04-14 19:47:37 +0800

    035-search-Insert

--- leetcode/035-search-Insert.go
+++ leetcode/035-search-Insert.go
@@ -1,6 +1,20 @@
 package leetcode
 
 func searchInsert(nums []int, target int) int {
+	l := 0
+	r := len(nums) - 1
 
-	return 0
+	//闭区间[l,r]
+	for l <= r {
+		mid := l + (r-l)>>1
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			//左侧区间
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return r + 1
 }
